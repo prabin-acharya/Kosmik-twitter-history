609c6e9a7fc562878bf87bb0cbd93056473fef04
Prabin Acharya <acharyaprabin101@gmail.com>
2022-12-05 23:47:11 +0545

    style: make tweets inline

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -25,70 +25,71 @@ interface Props {
 }
 
 export const Tweet: NextPage<Props> = ({ tweet }) => {
-  console.log(tweet.created_at, formatDate(tweet.created_at));
   return (
-    <Link href={`https://twitter.com/${tweet.username}/status/${tweet.id}`}>
-      <div className={styles.tweet}>
-        <div className={styles.tweetHeader}>
-          <div className={styles.tweetHeaderLeft}>
-            <Image
-              className={styles.tweetHeaderAvatar}
-              src={tweet.profile_image_url}
-              alt="avatar"
-              width={30}
-              height={30}
-            />
-            <div className={styles.tweetHeaderName}>
-              <a href={`https://twitter.com/${tweet.username}`}>
-                <strong>{tweet.name}</strong>
-              </a>
-              <span className={styles.tweetHeaderHandle}>
-                @{tweet.username}
+    <div className={styles.container}>
+      <Link href={`https://twitter.com/${tweet.username}/status/${tweet.id}`}>
+        <div className={styles.tweet}>
+          <div className={styles.tweetHeader}>
+            <div className={styles.tweetHeaderLeft}>
+              <Image
+                className={styles.tweetHeaderAvatar}
+                src={tweet.profile_image_url}
+                alt="avatar"
+                width={30}
+                height={30}
+              />
+              <div className={styles.tweetHeaderName}>
+                <a href={`https://twitter.com/${tweet.username}`}>
+                  <strong>{tweet.name}</strong>
+                </a>
+                <span className={styles.tweetHeaderHandle}>
+                  @{tweet.username}
+                </span>
+              </div>
+            </div>
+
+            <div className={styles.tweetHeaderRight}>
+              <span className={styles.tweetHeaderDate}>
+                {formatDate(tweet.created_at)}
               </span>
             </div>
           </div>
 
-          <div className={styles.tweetHeaderRight}>
-            <span className={styles.tweetHeaderDate}>
-              {formatDate(tweet.created_at)}
-            </span>
+          <div className={styles.tweetBody}>
+            <p className={styles.tweetText}>{tweet.text}</p>
+          </div>
+
+          <div className={styles.tweetFooter}>
+            <div className={styles.tweetFooterLike}>
+              <span className={styles.tweetFooterLikeCount}>
+                {tweet.public_metrics.like_count}
+              </span>
+              <span className={styles.tweetFooterLikeLabel}>Likes</span>
+            </div>
+
+            <div className={styles.tweetFooterReply}>
+              <span className={styles.tweetFooterReplyCount}>
+                {tweet.public_metrics.reply_count}
+              </span>
+              <span className={styles.tweetFooterReplyLabel}>Replies</span>
+            </div>
+
+            <div className={styles.tweetFooterRetweet}>
+              <span className={styles.tweetFooterRetweetCount}>
+                {tweet.public_metrics.retweet_count}
+              </span>
+              <span className={styles.tweetFooterRetweetLabel}>Retweets</span>
+            </div>
+
+            <div className={styles.tweetFooterQuote}>
+              <span className={styles.tweetFooterQuoteCount}>
+                {tweet.public_metrics.quote_count}
+              </span>
+              <span className={styles.tweetFooterQuoteLabel}>Quotes</span>
+            </div>
           </div>
         </div>
-
-        <div className={styles.tweetBody}>
-          <p className={styles.tweetText}>{tweet.text}</p>
-        </div>
-
-        <div className={styles.tweetFooter}>
-          <div className={styles.tweetFooterLike}>
-            <span className={styles.tweetFooterLikeCount}>
-              {tweet.public_metrics.like_count}
-            </span>
-            <span className={styles.tweetFooterLikeLabel}>Likes</span>
-          </div>
-
-          <div className={styles.tweetFooterReply}>
-            <span className={styles.tweetFooterReplyCount}>
-              {tweet.public_metrics.reply_count}
-            </span>
-            <span className={styles.tweetFooterReplyLabel}>Replies</span>
-          </div>
-
-          <div className={styles.tweetFooterRetweet}>
-            <span className={styles.tweetFooterRetweetCount}>
-              {tweet.public_metrics.retweet_count}
-            </span>
-            <span className={styles.tweetFooterRetweetLabel}>Retweets</span>
-          </div>
-
-          <div className={styles.tweetFooterQuote}>
-            <span className={styles.tweetFooterQuoteCount}>
-              {tweet.public_metrics.quote_count}
-            </span>
-            <span className={styles.tweetFooterQuoteLabel}>Quotes</span>
-          </div>
-        </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 };
