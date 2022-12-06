14e553aaa349964ba7eeffe7bdd9cd382d4af96d
Prabin Acharya <acharyaprabin101@gmail.com>
2022-12-06 13:10:28 +0545

    api home :select two random accounts and get random tweets

--- pages/api/auth/home.ts
+++ pages/api/auth/home.ts
@@ -1,7 +1,29 @@
 import { ObjectId } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
-import { TwitterApi, TwitterV2IncludesHelper } from "twitter-api-v2";
+import {
+  TweetPublicMetricsV2,
+  TwitterApi,
+  TwitterV2IncludesHelper,
+} from "twitter-api-v2";
 import clientPromise from "../../../lib/mongodb";
+
+function getRandomItems(array: any[]) {
+  if (array.length < 3) {
+    return;
+  }
+
+  // Create a copy of the array so we don't modify the original
+  const arrayCopy = array.slice();
+
+  // Shuffle the array in place using the Fisher-Yates shuffle algorithm
+  for (let i = arrayCopy.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arrayCopy[i], arrayCopy[j]] = [arrayCopy[j], arrayCopy[i]];
+  }
+
+  // Return the first three items from the shuffled array
+  return arrayCopy;
+}
 
 interface User {
   id: string;
@@ -51,52 +73,81 @@ export default async function handler(
 
   const user: { data: User; following?: User[] } =
     await refreshedClient.v2.me();
-
   const following = await refreshedClient.v2.following(user.data.id);
-
   user.following = following.data;
 
-  const firstId = following.data[8];
+  const randomizedFollowing = getRandomItems(following.data)?.slice(0, 2);
 
-  const oldTweets = await refreshedClient.v2.userTimeline(firstId.id, {
-    max_results: 9,
-    end_time: "2020-07-05T00:00:00.52Z",
-    "tweet.fields": ["id", "text", "created_at", "public_metrics", "source"],
-    expansions: [
-      "author_id",
-      "entities.mentions.username",
-      "referenced_tweets.id",
-      "in_reply_to_user_id",
-    ],
-    "user.fields": [
-      "id",
-      "name",
-      "description",
-      "public_metrics",
-      "profile_image_url",
-    ],
+  const randomizedFollowingOrderedTweets:
+    | Promise<
+        | {
+            id: string;
+            text: string;
+            created_at: string | undefined;
+            public_metrics: TweetPublicMetricsV2 | undefined;
+            profile_image_url: string | undefined;
+            username: string | undefined;
+            name: string | undefined;
+          }[]
+        | undefined
+      >[]
+    | undefined = randomizedFollowing?.map(async (user) => {
+    try {
+      const userTimeline = await refreshedClient.v2.userTimeline(user.id, {
+        max_results: 5,
+        end_time: "2020-07-05T00:00:00.52Z",
+        "tweet.fields": [
+          "id",
+          "text",
+          "created_at",
+          "public_metrics",
+          "source",
+        ],
+        expansions: [
+          "author_id",
+          "entities.mentions.username",
+          "referenced_tweets.id",
+          "in_reply_to_user_id",
+        ],
+        "user.fields": [
+          "id",
+          "name",
+          "description",
+          "public_metrics",
+          "profile_image_url",
+        ],
+      });
+
+      const includes = new TwitterV2IncludesHelper(userTimeline);
+
+      const oldTweetsOnly = [];
+      for (const tweet of userTimeline.tweets) {
+        const user = includes.author(tweet);
+        oldTweetsOnly.push({
+          id: tweet.id,
+          text: tweet.text,
+          created_at: tweet.created_at,
+          public_metrics: tweet.public_metrics,
+          profile_image_url: user?.profile_image_url,
+          username: user?.username,
+          name: user?.name,
+        });
+      }
+
+      return oldTweetsOnly;
+    } catch (error) {
+      console.log(error);
+    }
   });
 
-  const includes = new TwitterV2IncludesHelper(oldTweets);
+  if (!randomizedFollowingOrderedTweets) return res.status(200).json({});
 
-  const oldTweetsOnly = [];
+  const allOldTweetsOnly = await Promise.all(randomizedFollowingOrderedTweets);
 
-  for (const tweet of oldTweets.tweets) {
-    const user = includes.author(tweet);
-    oldTweetsOnly.push({
-      id: tweet.id,
-      text: tweet.text,
-      created_at: tweet.created_at,
-      public_metrics: tweet.public_metrics,
-      profile_image_url: user?.profile_image_url,
-      username: user?.username,
-      name: user?.name,
-    });
-  }
+  console.log("++++++++++++++++++++++++", allOldTweetsOnly);
 
   res.status(200).json({
     user: user,
-    oldTweets: oldTweetsOnly,
-    oldTweetsDetail: oldTweets,
+    oldTweets: allOldTweetsOnly,
   });
 }
