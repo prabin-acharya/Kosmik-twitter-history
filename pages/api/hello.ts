009aae197ecfc9c41378819ac93c97b6b9e17c08
Prabin Acharya <acharyaprabin101@gmail.com>
2022-12-09 02:30:30 +0545

    add test api

---
+++ pages/api/hello.ts
@@ -0,0 +1,25 @@
+import { ObjectId } from "mongodb";
+import { NextApiRequest, NextApiResponse } from "next";
+import {
+  TweetPublicMetricsV2,
+  TwitterApi,
+  TwitterV2IncludesHelper,
+} from "twitter-api-v2";
+
+import clientPromise from "../../lib/mongodb";
+
+interface User {
+  id: string;
+  name: string;
+  username: string;
+  profile_image_url?: string;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.status(200).json({
+    hello: "Hello API",
+  });
+}
