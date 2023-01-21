b30113d5265b8c7cb8473d2492a2657bdee556b9
Prabin Acharya <acharyaprabin101@gmail.com>
2023-01-21 21:13:52 +0545

    delete state,code from db after successful auth

--- pages/api/auth/callback.ts
+++ pages/api/auth/callback.ts
@@ -25,6 +25,7 @@ export default async function handler(
   const {
     client: loggedClient,
     accessToken,
+    expiresIn,
     refreshToken,
   } = await twitterClient.loginWithOAuth2({
     code: code as string,
@@ -33,52 +34,11 @@ export default async function handler(
   });
 
   res.setHeader("Set-Cookie", [
-    `access_Token=${accessToken}; HttpOnly; SameSite=Strict; Path=/`,
+    `access_Token=${accessToken}; HttpOnly; SameSite=Strict;Max-Age=${expiresIn}first; Path=/`,
     `refresh_Token=${refreshToken}; HttpOnly; SameSite=Strict; Path=/`,
   ]);
 
+  await db.collection("authTwitter").deleteOne({ state: state });
+
   res.redirect(process.env.APP_URL as string);
 }
-
-// import type { NextApiRequest, NextApiResponse } from "next";
-// import { TwitterApi } from "twitter-api-v2";
-
-// export default function handler(req: NextApiRequest, res: NextApiResponse) {
-//   // Extract state and code from query string
-//   const { state, code } = req.query;
-//   // Get the saved codeVerifier from session
-//   const { codeVerifier, state: sessionState } = req.session;
-
-//   if (!codeVerifier || !state || !sessionState || !code) {
-//     return res.status(400).send("You denied the app or your session expired!");
-//   }
-//   if (state !== sessionState) {
-//     return res.status(400).send("Stored tokens didnt match!");
-//   }
-
-//   // Obtain access token
-//   const client = new TwitterApi({
-//     clientId: CLIENT_ID,
-//     clientSecret: CLIENT_SECRET,
-//   });
-
-//   client
-//     .loginWithOAuth2({ code, codeVerifier, redirectUri: CALLBACK_URL })
-//     .then(
-//       async ({
-//         client: loggedClient,
-//         accessToken,
-//         refreshToken,
-//         expiresIn,
-//       }) => {
-//         // {loggedClient} is an authenticated client in behalf of some user
-//         // Store {accessToken} somewhere, it will be valid until {expiresIn} is hit.
-//         // If you want to refresh your token later, store {refreshToken} (it is present if 'offline.access' has been given as scope)
-
-//         // Example request
-//         const { data: userObject } = await loggedClient.v2.me();
-//       }
-//     )
-//     .catch(() => res.status(403).send("Invalid verifier or access tokens!"));
-//   //   res.status(200).json({ name: "John Doe" });
-// }
